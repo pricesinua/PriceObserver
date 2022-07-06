c47a68bed959d8dd85855f53f254e44875d858fd
qualterz <qualterz@tutamail.com>
2022-07-06 16:28:03 +0300

    Specify pricestamp do not have primary key

--- PriceObserver.Persistance/AppDbContext.cs
+++ PriceObserver.Persistance/AppDbContext.cs
@@ -8,4 +8,9 @@ public class AppDbContext : DbContext
     public DbSet<PriceStamp> PriceStamps { get; set; }
 
     public AppDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder model)
+    {
+        model.Entity<PriceStamp>().HasNoKey();
+    }
 }
