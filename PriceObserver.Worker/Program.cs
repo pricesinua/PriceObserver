002cd96a87848896fdb1873a9acb99c846331fa3
qualterz <qualterz@tutamail.com>
2022-07-06 16:23:10 +0300

    Register database context
    Resolves #8

--- PriceObserver.Worker/Program.cs
+++ PriceObserver.Worker/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PriceObserver.Persistance;
 using PriceObserver.Worker;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddDbContextPool<AppDbContext>(options => {
+            options.UseSqlite("Data Source=appdata.sqlite");
+        });
         services.AddHostedService<Worker>();
     })
     .Build();
