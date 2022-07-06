ba5e7c7aa845f4229c94c6b91e72f359f1e7ab6c
qualterz <qualterz@tutamail.com>
2022-07-06 16:20:32 +0300

    Remove non-existing table

--- PriceObserver.Persistance/AppDbContext.cs
+++ PriceObserver.Persistance/AppDbContext.cs
@@ -5,7 +5,6 @@ using PriceObserver.Persistance.Data;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<ProductEntry> ProductEntries { get; set; }
     public DbSet<PriceStamp> PriceStamps { get; set; }
 
     public AppDbContext(DbContextOptions options) : base(options) { }
