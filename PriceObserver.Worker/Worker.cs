cc075602b8bba67e9296cb68bfcab8d61a27e246
qualterz <qualterz@tutamail.com>
2022-07-08 14:29:20 +0300

    Do not use underscore in field name

--- PriceObserver.Worker/Worker.cs
+++ PriceObserver.Worker/Worker.cs
@@ -2,18 +2,18 @@ namespace PriceObserver.Worker;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
+    private readonly ILogger<Worker> logger;
 
     public Worker(ILogger<Worker> logger)
     {
-        _logger = logger;
+        this.logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
         while (!cancellationToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             await Task.Delay(1000, cancellationToken);
         }
     }
