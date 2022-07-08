df092832f17954ed9556778d3985cee1ed8de7c8
qualterz <qualterz@tutamail.com>
2022-07-08 17:26:11 +0300

    Log environment mode

--- PriceObserver.Worker/Program.cs
+++ PriceObserver.Worker/Program.cs
@@ -2,21 +2,33 @@ using Microsoft.EntityFrameworkCore;
 using PriceObserver.Persistance;
 using PriceObserver.Worker;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((host, services) =>
-    {
-        var connectionString = host.Configuration.GetValue<string>("ConnectionString");
-        services.AddDbContextPool<AppDbContext>(options => {
-            options.UseSqlite(connectionString);
-        });
-        services.AddHostedService<Worker>();
-    })
-    .Build();
-
-using (var scope = host.Services.CreateScope())
+internal class Program
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
+    private static async Task Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((host, services) =>
+            {
+                var connectionString = host.Configuration.GetValue<string>("ConnectionString");
+                services.AddDbContextPool<AppDbContext>(options =>
+                {
+                    options.UseSqlite(connectionString);
+                });
+                services.AddHostedService<Worker>();
+            })
+            .Build();
 
-await host.RunAsync();
+        using (var scope = host.Services.CreateScope())
+        {
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            var environment = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+
+            logger.LogInformation($"Environment mode: {environment.EnvironmentName}");
+
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+        }
+
+        await host.RunAsync();
+    }
+}
