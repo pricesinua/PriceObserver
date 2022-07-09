6d34597894f3ccb2cda28b166073c9c9c7470127
qualterz <qualterz@tutamail.com>
2022-07-09 22:51:54 +0300

    Store product ean instead of product id

--- PriceObserver.Persistance/Data/PriceStamp.cs
+++ PriceObserver.Persistance/Data/PriceStamp.cs
@@ -1,9 +1,11 @@
+using System.Numerics;
+
 namespace PriceObserver.Persistance.Data;
 
 public class PriceStamp
 {
     public long Id { get; set; }
-    public int ProductId { get; set; }
+    public string ProductEan { get; set; }
     public int StoreId { get; set; }
     public int Price { get; set; }
     public short Currency { get; set; }
---
+++ PriceObserver.Persistance/Migrations/20220709194830_ReplaceProductIdWithProductEan.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceObserver.Persistance.Migrations
+{
+    public partial class ReplaceProductIdWithProductEan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "PriceStamps");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductEan",
+                table: "PriceStamps",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductEan",
+                table: "PriceStamps");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "PriceStamps",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
