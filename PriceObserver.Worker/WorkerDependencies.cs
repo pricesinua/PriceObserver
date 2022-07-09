3717053a67903ea87bf5374793e4f2e1698919bb
qualterz <qualterz@tutamail.com>
2022-07-09 21:11:30 +0300

    Do not dispose scope

--- PriceObserver.Worker/WorkerDependencies.cs
+++ PriceObserver.Worker/WorkerDependencies.cs
@@ -7,7 +7,7 @@ public partial class Worker : IRecurringAction
 {
     public Worker(IServiceProvider serviceProvider)
     {
-        using var scope = serviceProvider.CreateScope();
+        var scope = serviceProvider.CreateScope();
         var scopedServiceProvider = scope.ServiceProvider;
 
         this.logger = scopedServiceProvider.GetRequiredService<ILogger<Worker>>();
