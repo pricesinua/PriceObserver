0f6cb50281f14d3209c7de41808df6d031202d07
qualterz <qualterz@tutamail.com>
2022-07-09 21:12:46 +0300

    Implement basic price parsing
    Closes issue #11

--- PriceObserver.Worker/Worker.cs
+++ PriceObserver.Worker/Worker.cs
@@ -1,6 +1,7 @@
 using CronBackgroundServices;
 using Cronos;
 using PriceObserver.Persistance;
+using PriceObserver.Persistance.Data;
 
 namespace PriceObserver.Worker;
 
@@ -12,14 +13,43 @@ public partial class Worker : IRecurringAction
 
     public string Cron => configuration.GetValue<string>("ParseSchedule");
 
-    public Task Process(CancellationToken stoppingToken)
+    public async Task Process(CancellationToken stoppingToken)
     {
+        var client = new ApiZakazUa.Client();
+
+        var stores = await client.GetStoresAsync();
+
+        foreach (var store in stores)
+        {
+            logger.LogTrace($"Processing store: {store.Id} {store.Name}.");
+            var categories = await client.GetCategoriesAsync(store.Id);
+
+            foreach (var category in categories)
+            {
+                logger.LogTrace($"Processing category: {category.Id} {category.Title}.");
+                var products = await client.GetProductsAsync(store.Id, category.Id);
+
+                foreach (var product in products)
+                {
+                    logger.LogTrace($"Processing product: {product.Ean} {product.Title}.");
+                    appDbContext.PriceStamps.Add(new PriceStamp()
+                    {
+                        StoreId = store.Id,
+                        ProductId = product.Ean.GetHashCode(),
+                        Price = product.Price,
+                        Currency = short.Parse(NodaMoney.Currency.FromCode(product.Currency.ToUpper()).Number),
+                        Timestamp = DateTime.UtcNow,
+                    });
+                }
+            }
+
+            await appDbContext.SaveChangesAsync();
+        }
+
         var cronExpression = CronExpression.Parse(Cron, CronFormat.IncludeSeconds);
 
         var nextTime = cronExpression.GetNextOccurrence(DateTime.UtcNow);
 
         logger.LogInformation($"Next parse will start at: {nextTime?.ToLocalTime()}");
-
-        return Task.CompletedTask;
     }
 }
